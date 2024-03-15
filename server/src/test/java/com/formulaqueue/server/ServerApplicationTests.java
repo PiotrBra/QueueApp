c0024d10719a20158134ccca80b234c73b00db76
Piotr Branewski <127198014+PiotrBra@users.noreply.github.com>
2024-03-15 18:51:49 +0100

    Add files via upload

---
+++ server/src/test/java/com/formulaqueue/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formulaqueue.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
