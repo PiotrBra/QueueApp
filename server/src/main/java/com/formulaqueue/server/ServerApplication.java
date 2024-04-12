2d4937fa67d1a3adc42309248f0f19f869303c3e
Piotr Branewski <pbranewski@student.agh.edu.pl>
2024-04-12 19:49:07 +0200

    json

--- server/src/main/java/com/formulaqueue/server/ServerApplication.java
+++ server/src/main/java/com/formulaqueue/server/ServerApplication.java
@@ -1,9 +1,7 @@
 package com.formulaqueue.server;
 
-import com.formulaqueue.server.utils.JsonLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.List;
 
 @SpringBootApplication
 public class ServerApplication {
